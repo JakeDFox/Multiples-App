e47c93044c0d5d4a3df46cc169e5b07d5ef79585
Jacob Fox <jacobfox@Jacobs-MacBook-Pro-2.local>
2017-05-12 10:37:48 +0100

    Adding multiple commits and files

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -9,17 +9,88 @@
 import UIKit
 
 class ViewController: UIViewController {
+        
+    //Properties
+    
+    var currentMultiple = 0
+    var currentFirstNumber = 0
+    var currentSum = 0
+    
+    //Outlets
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var Multiples: UIImageView!
+
+    @IBOutlet weak var WhatMultiplesTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var addLbl: UILabel!
+    
+    @IBAction func onAddBtnPressed(Sender: UIButton!){
+        updateMultiplesLbl()
+        currentFirstNumber = currentSum
+        
+        if isGameOver() {
+                restartGame()
+        }
     }
+    
+    
+    @IBAction func onPlayBtnPressed(Sender: UIButton!){
+        
+        if WhatMultiplesTxt.text != nil && WhatMultiplesTxt.text != ""{
+            
+            Multiples.isHidden = true
+            
+            WhatMultiplesTxt.isHidden = true
+            
+            playBtn.isHidden = true
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            addLbl.isHidden = false
+
+            addBtn.isHidden = false
+            
+            currentMultiple = Int(WhatMultiplesTxt.text!)!
+            currentFirstNumber = 0
+   
+        }
+    
     }
+    
 
+        func isGameOver() ->Bool {
+            if currentSum >= 500 {
+                return true
+            } else {
+                return false
+            }
+            }
+        
+        func restartGame(){
+        
+            currentMultiple = 0
+            currentSum = 0
+            currentFirstNumber = 0
+            WhatMultiplesTxt.text = ""
+        
+        Multiples.isHidden = false
+        
+        WhatMultiplesTxt.isHidden = false
+        
+        playBtn.isHidden = false
+        
+        addLbl.isHidden = true
+        
+        addBtn.isHidden = true
+            
+        updateMultiplesLbl()
 
+        }
+    
+    func updateMultiplesLbl(){
+        currentSum = (currentFirstNumber) + (currentMultiple)
+        addLbl.text = "\(currentFirstNumber) + \(currentMultiple) = \(currentSum)"
+    
 }
 
+}
